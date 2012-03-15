db89f0d3c77dfcf332184e650203f406740088c8
ahmd refat <ahmd.refat.twfik@gmail.com>
2012-03-15 18:44:15 +0200

    edit

--- db/migrate/20120306152746_create_comments.rb
+++ db/migrate/20120306152746_create_comments.rb
@@ -5,6 +5,7 @@ class CreateComments < ActiveRecord::Migration
     	t.integer :post_id
     	t.integer :person_id
      	 t.timestamps
+       
     end
     add_index :comments,:post_id
     add_index :comments,:person_id
