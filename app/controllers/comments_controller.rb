fc218e6d7c5930a7bd45d46f9e5e4838774e2969
Karim Husseiny <just.azzurri@gmail.com>
2012-03-18 18:14:07 +0200

    Posting

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,17 @@
 class CommentsController < ApplicationController
 	before_filter :authenticate_user
+
+
+	def create
+			@comment = Comment.new(params[:comment])
+			@comment.person_id = User.find(session[:user_id]).person.id
+			@comment.post_id= params[:post_id]
+    	if @comment.save
+      		# redirect_to(:controller => 'access', :action => 'login')
+      		# render text: "you created comment by #{@comment.person.user.username}"
+      		redirect_to :controller => 'posts' , :action => 'new'
+    	else
+      		render text: "Something worng happen while Commenting"
+      end
+	end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,21 @@
 class PostsController < ApplicationController
 	before_filter :authenticate_user
+
+	def new
+		@post = Post.new
+		@posts = Post.order("posts.created_at DESC")
+	end
+
+	def create
+			@post = Post.new(params[:post])
+			@post.post_type= "status"
+			@post.person_id = User.find(session[:user_id]).person.id
+    	if @post.save
+      		# redirect_to(:controller => 'access', :action => 'login')
+      		# render text: "you created post by #{@post.person.user.username}"
+      		redirect_to :action => 'new'
+    	else
+      		render text: "Something worng happen while posting"
+      end
+	end
 end
