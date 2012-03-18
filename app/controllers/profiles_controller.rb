72432c5c47d1fb1fa538cf1a3bedc273a3a63406
ahmd refat <ahmd.refat.twfik@gmail.com>
2012-03-18 23:14:33 +0200

    edit profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -9,7 +9,19 @@ class ProfilesController < ApplicationController
 
 		@profile = Profile.find_by_user_id @current_user.id
 		if @profile
-			@profile.update_attributes(params[:profile])
+			@profile.update_attributes(:first_name => params[:first_name], :last_name => params[:last_name], :location => params[:location], :birthdate => params[:birthdate], :bio => params[:bio])
+			tags = params[:tags].split ", and "
+			@profile.tags = []
+			tags.each do |t|
+				# fix uniquness
+				tag = Tag.create :text => t
+				@profile.tags << tag
+			end
+			@profile.save!
+
+			@current_user.email = params[:email]
+			@current_user.save!
+
 			redirect_to(:controller => 'profiles', :action => 'index')	
 		else
 			@profile = Profile.new params[:profile]
@@ -30,6 +42,13 @@ class ProfilesController < ApplicationController
 			@profile = Profile.new
 		else
 			@profile
+			unless @profile.tags.empty?
+				@tags = []
+				Profile.first.tags.each do |tag| @tags << tag.text end
+			else
+				@tags = []
+			end
+
 		end
 	end
 
