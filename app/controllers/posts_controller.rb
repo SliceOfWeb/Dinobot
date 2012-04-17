41a57e639eb585ffeaaf6497f7c22ca2a426ee56
ahmd refat <ahmd.refat.twfik@gmail.com>
2012-04-18 00:36:09 +0200

    nothing changed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,9 +2,9 @@ class PostsController < ApplicationController
 	before_filter :authenticate_user
 
 	def create
-			@post = Post.new(params[:post])
-			@post.post_type= "status"
-			@post.person_id = @current_user.person.id
+		@post = Post.new(params[:post])
+		@post.post_type= "status"
+		@post.person_id = @current_user.person.id
     	if @post.save
     		Action.create(:target_type => 'post', :target_id => @post.id, :upvote_count => 0)
       		redirect_to home_path
