9578a0fe6fcbba8ed8152ece6d44a635510ef132
Karim Husseiny <just.azzurri@gmail.com>
2012-05-12 03:04:11 +0200

    Edit Thumbinals and Add styles to paperclip attachment

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -7,6 +7,14 @@ class ImagesController < ApplicationController
 
 	def create
 		@image = Image.create params[:image]
+		@album = Album.find(@image.album_id)
+		if @album.images[0] == @image
+			@album.cover_url = @image.image.url
+		end
 		redirect_to album_path(@image.album_id)
+	end
+
+	def show
+		@image = Image.find params[:id]
 	end
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,4 @@
 class Image < ActiveRecord::Base
 	belongs_to :album
-	has_attached_file :image
-end
+	has_attached_file :image, :styles => {:thumb => '150x150#', :album_cover => '260x180#'}
+end 
